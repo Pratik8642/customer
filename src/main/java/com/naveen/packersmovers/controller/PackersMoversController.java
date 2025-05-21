7159708d8a1e8a286d8ed13d2e5910d00e0fa9cb
Pratik Vandariya <vandariyapratik8642@gmail.com>
2025-05-21 16:49:11 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -26,6 +26,6 @@ public class PackersMoversController {
         
         
         @GetMapping("/ping")
-        public String ping() {  return "Hi, This is Mishali Vandariya!";}             
+        public String ping() {  return "Hi, This is Pratik Vandariya!";}             
 
     }
